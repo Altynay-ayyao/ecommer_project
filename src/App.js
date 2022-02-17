2867f436f84725a14ef9d05141732560dde6f0a1
Altynay-ayyao <77488933+Altynay-ayyao@users.noreply.github.com>
2022-02-17 23:55:17 +0200

    third commit

--- src/App.js
+++ src/App.js
@@ -1,35 +1,35 @@
-import data from './data';
 import React from 'react';
-import Product from './components/Product';
+import HomeScreen from './screens/HomeScreen';
+import ProductScreen from './screens/ProductScreen';
+import { BrowserRouter, Route } from 'react-router-dom';
 
 function App() {
   return (
-    <div className="App">
-      <div className="grid-container">
-        <header className="row">
-          <div>
-            <a className="brand" href="/">
-              Droppe Xmas
-            </a>
-          </div>
-          <div>
-            <a href="/cart">Cart</a>
-            <a href="/signin">Sign In</a>
-          </div>
-        </header>
-        <main>
-          <div className="row center">
-            {data.products.map((product) => (
-              <Product key={product._id} product={product} />
-            ))}
-          </div>
-        </main>
-        <footer className="row center">
-          All right reserved (photo copy right belongs to vtech company from
-          amazon.de)
-        </footer>
+    <BrowserRouter>
+      <div className="App">
+        <div className="grid-container">
+          <header className="row">
+            <div>
+              <a className="brand" href="/">
+                Droppe Xmas
+              </a>
+            </div>
+            <div>
+              <a href="/cart">Cart</a>
+              <a href="/signin">Sign In</a>
+            </div>
+          </header>
+          <main>
+            <Route path="/product/:id" component={ProductScreen}></Route>
+            <Route path="/" component={HomeScreen} exact></Route>
+          </main>
+          <footer className="row center">
+            All right reserved (photo copy right belongs to vtech company from
+            amazon.de)
+          </footer>
+        </div>
       </div>
-    </div>
+    </BrowserRouter>
   );
 }
 
