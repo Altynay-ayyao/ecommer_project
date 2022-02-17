8c7017b18bc853647da839e612e86046ad2f93bc
Altynay-ayyao <77488933+Altynay-ayyao@users.noreply.github.com>
2022-02-17 22:26:13 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,64 @@
-import logo from './logo.svg';
-import './App.css';
+import data from './data';
+import React from 'react';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="grid-container">
+        <header className="row">
+          <div>
+            <a className="brand" href="/">
+              Droppe Xmas
+            </a>
+          </div>
+          <div>
+            <a href="/cart">Cart</a>
+            <a href="/signin">Sign In</a>
+          </div>
+        </header>
+        <main>
+          <div className="row center">
+            {data.products.map((product) => (
+              <div key={product._id} className="card">
+                <a href={`/product/${product._id}`}>
+                  <img
+                    className="medium"
+                    src={product.image}
+                    alt={product.name}
+                  />
+                </a>
+                <div className="card-body">
+                  <a href={`/product/${product._id}`}>
+                    <h2>Bear</h2>
+                    <div className="rating">
+                      <span>
+                        <i className="fa fa-star"></i>
+                      </span>
+                      <span>
+                        <i className="fa fa-star"></i>
+                      </span>
+                      <span>
+                        <i className="fa fa-star"></i>
+                      </span>
+                      <span>
+                        <i className="fa fa-star"></i>
+                      </span>
+                      <span>
+                        <i className="fa fa-star-half"></i>
+                      </span>
+                    </div>
+                    <div className="price">{product.price}€</div>
+                  </a>
+                </div>
+              </div>
+            ))}
+          </div>
+        </main>
+        <footer className="row center">
+          All right reserved (photo copy right belongs to vtech company from
+          amazon.de)
+        </footer>
+      </div>
     </div>
   );
 }
